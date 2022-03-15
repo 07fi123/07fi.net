cdb54266d6960f83699f4db3bf3601b9f9d04b09
Austin <07fi123@gmail.com>
2022-03-14 23:36:16 -0500

    fix headers mobile fixes

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,7 +4,7 @@ import style from './style.scss';
 const Home = () => (
 	<div class={style.home}>
 		<h1>Hello, My name is Austin.</h1>
-		<img src="/assets/mug.jpeg"/>
+		<img src="/assets/mug.jpeg" alt="profile-image"/>
 		<p>I build excellent software for organizations and individuals.<br/> What could I build <span class={style.forYou}>for you?</span></p>
 		<p><a class={style.book} href="https://book.morgen.so/07fi12392/open-hours">Book a Consultation</a></p>
 	</div>
