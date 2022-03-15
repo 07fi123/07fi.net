7728e364130a727a400866f9af67df4ab23541da
Austin <07fi123@gmail.com>
2022-03-14 23:19:49 -0500

    starting to look nice

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,13 @@
 import { h } from 'preact';
 import { Link } from 'preact-router/match';
-import style from './style.css';
+import style from './style.scss';
 
 const Header = () => (
 	<header class={style.header}>
-		<h1>Preact App</h1>
+		<h1>07fi.net</h1>
 		<nav>
-			<Link activeClassName={style.active} href="/">Home</Link>
-			<Link activeClassName={style.active} href="/profile">Me</Link>
-			<Link activeClassName={style.active} href="/profile/john">John</Link>
+			<Link activeClassName={style.active} href="/">Hello</Link>
+			<a activeClassName={style.active} href="https://github.com/07fi123">Github</a>
 		</nav>
 	</header>
 );
---
+++ src/components/widgets/morgen.js
@@ -0,0 +1,9 @@
+import { h } from 'preact';
+
+const Morgen = () => (
+	<div id="morgen">
+        <iframe src="https://book.morgen.so/07fi12392/open-hours" width="100%" height="700px" style="border: none" />
+	</div>
+)
+
+export default Morgen;
